88b5ee238d92587f5863e2e15c5a5ff83bfd842a
devaspirant0510 <seungho020510@gmail.com>
2024-08-09 17:29:43 +0900

    feat : ios background processing 대응

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,21 +8,11 @@ import workmanager
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      
+  WorkmanagerPlugin.registerBGProcessingTask(withIdentifier: "task-identifier")
 
-      // In AppDelegate.application method
-      WorkmanagerPlugin.registerBGProcessingTask(withIdentifier: "task-identifier")
-
-      // Register a periodic task in iOS 13+
-      WorkmanagerPlugin.registerPeriodicTask(withIdentifier: "be.tramckrijte.workmanagerExample.iOSBackgroundAppRefresh", frequency: NSNumber(value: 20 * 60))
 
     GeneratedPluginRegistrant.register(with: self)
-      if #available(iOS 10.0, *) {
-        UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
-      }
-      // Register a periodic task in iOS 13+
-      UIApplication.shared.setMinimumBackgroundFetchInterval(TimeInterval(60*15))
-      
-
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
